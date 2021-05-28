01659bec0a137f49ccccc7d1305272888e1ed10f
saileshchauhan <chauhansailesh68@gmail.com>
2021-05-28 09:31:00 +0530

    UC1-create list add 25 records

---
+++ ProductReviewManagementUsingLINQ/ProductReview.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductReviewManagementUsingLINQ
+{
+ public class ProductReview
+    {
+        public int ProductID { get; set; }
+        public int UserID { get; set; }
+        public double Rating { get; set; }
+        public string Review { get; set; }
+        public bool IsLike { get; set; }
+        public ProductReview(int productId, int userId, double rating, string review, bool islike)
+        {
+            ProductID = productId;
+            UserID = userId;
+            Rating = rating;
+            Review = review;
+            IsLike = islike;
+        }
+    }
+}
+
---
+++ ProductReviewManagementUsingLINQ/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProductReviewManagementUsingLINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //UC-1
+            List<ProductReview> productReviews = new List<ProductReview>() 
+            { new ProductReview(1,7,3,"Suggest Buy",true)};
+
+            productReviews.Add(new ProductReview(2,  15, 1, "Defective", false));
+            productReviews.Add(new ProductReview(5,  15, 4, "Good", true));
+            productReviews.Add(new ProductReview(78, 23, 2, "Suitable", true));
+            productReviews.Add(new ProductReview(2,  21, 5, "Good", true));
+            productReviews.Add(new ProductReview(3,  16, 1, "Defective", false));
+            productReviews.Add(new ProductReview(13, 19, 5, "Excellent", true));
+            productReviews.Add(new ProductReview(2,  17, 3, "Average", true));
+            productReviews.Add(new ProductReview(101,15, 5, "Happy", true));
+            productReviews.Add(new ProductReview(13, 56, 5, "Happy", true));
+            productReviews.Add(new ProductReview(6,  107, 1, "Defective", false));
+            productReviews.Add(new ProductReview(9,  14, 5, "Excellent", true));
+            productReviews.Add(new ProductReview(6,  15, 1, "Defective", false));
+            productReviews.Add(new ProductReview(17, 16, 5, "Excellent", true));
+            productReviews.Add(new ProductReview(33, 14, 3, "Average", false));
+            productReviews.Add(new ProductReview(13, 14, 7, "Excellent", true));
+            productReviews.Add(new ProductReview(5,  55, 3, "Average", true));
+            productReviews.Add(new ProductReview(17, 16, 1, "Defective", false));
+            productReviews.Add(new ProductReview(11, 10, 6, "Excellent", true));
+            productReviews.Add(new ProductReview(6, 56, 2, "Poor", false));
+            productReviews.Add(new ProductReview(6, 11, 3, "Average", false));
+            productReviews.Add(new ProductReview(10, 15, 5, "Good", true));
+            //UC-1
+        }
+    }
+}
